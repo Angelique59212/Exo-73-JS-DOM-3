54213605e2af3852e5ae4d29fece5ae14aa97337
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-11-18 11:05:13 +0100

    dom3

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -2,6 +2,10 @@ let colorEl = document.getElementById("trendy-color");
 let handEl  = document.getElementById("trendy-handwriting");
 let frameEl = document.getElementById("trendy-frame");
 
+colorEl.innerHTML = "deepink";
+handEl.innerHTML = "tablet";
+frameEl.style.border = "2px solid black";
+
 
 
 
--- exo 4/exercice.js
+++ exo 4/exercice.js
@@ -1,8 +1,22 @@
+let mondiv =document.createElement("div");
+mondiv.className = "planet";
+mondiv.style.background =  "darkgray";
+document.body.appendChild(mondiv);
+
+let secondDiv = document.createElement("div");
+secondDiv.className = "moon";
+document.body.appendChild(secondDiv);
 
 
+let thirdDiv = document.createElement("div");
+thirdDiv.className = "planet";
+thirdDiv.style.background = "blue";
+thirdDiv.style.boxShadow = "2px 2px 2px 2px white";
+document.body.appendChild(thirdDiv);
 
-
-
+let fourthDiv = document.createElement("div");
+fourthDiv.className = "moon";
+document.body.appendChild(fourthDiv);
 
 
 
